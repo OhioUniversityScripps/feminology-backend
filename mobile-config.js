a8413b03b459ad2e9d5c9b7d20f11aa306c6de25
Zach Perrault <zach.perrault@gmail.com>
2015-05-05 07:02:14 -0400

    Cleaning up the mobile-config.js file

--- mobile-config.js
+++ mobile-config.js
@@ -1,7 +1,4 @@
-App.setPreference('StatusBarOverlaysWebView', 'false');
-App.setPreference('StatusBarStyle', 'false');
-App.setPreference('StatusBarBackgroundColor', '#f7f7f7');
-
+// App metadata
 App.info({
 	id: 'edu.zp376411.feminology',
 	name: 'Feminology',
@@ -11,37 +8,27 @@ App.info({
 	website: 'http://feminologyapp.com'
 });
 
+
+// PhoneGap config.xml preferences
+App.setPreference('Orientation', 'portrait');
+App.setPreference('StatusBarOverlaysWebView', 'false');
+App.setPreference('StatusBarStyle', 'false');
+App.setPreference('StatusBarBackgroundColor', '#f7f7f7');
+
+// App media
 App.icons({
 	// iOS
-	'iphone': 'resources/icons/icon-60.png',
-	'iphone_2x': 'resources/icons/icon-60@2x.png',
-	// 'ipad': 'resources/icons/icon-72x72.png',
-	// 'ipad_2x': 'resources/icons/icon-72x72@2x.png',
-
-	// Android
-	// 'android_ldpi': 'resources/icons/icon-36x36.png',
-	// 'android_mdpi': 'resources/icons/icon-48x48.png',
-	// 'android_hdpi': 'resources/icons/icon-72x72.png',
-	// 'android_xhdpi': 'resources/icons/icon-96x96.png'
+	'iphone': 'resources/icons/icon-iphone.png',
+	'iphone_2x': 'resources/icons/icon-iphone_2x.png',
+	'iphone_3x': 'resources/icon/icon-iphone_3x.png',
 });
 
 App.launchScreens({
 	// iOS
-	'iphone': 'resources/splash/splash-320x480.png',
-	'iphone_2x': 'resources/splash/splash-320x480@2x.png',
-	'iphone5': 'resources/splash/splash-320x568@2x.png',
-	// 'ipad_portrait': 'resources/splash/splash-768x1024.png',
-	// 'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
-	// 'ipad_landscape': 'resources/splash/splash-1024x768.png',
-	// 'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
-
-	// Android
-	// 'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-	// 'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
-	// 'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-	// 'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
-	// 'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-	// 'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
-	// 'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
-	// 'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+	'iphone': 'resources/splash/splash-iphone.png',
+	'iphone_2x': 'resources/splash/splash-iphone_2x.png',
+	'iphone5': 'resources/splash/splash-iphone5.png',
+	'iphone6': 'resources/splash/splash-iphone6.png',
+	'iphone6p_portrait': 'resources/splash/splash-iphone6p_portrait.png',
+	'iphone6p_landscape': 'resources/splash/splash-iphone6p_landscape.png',
 });
