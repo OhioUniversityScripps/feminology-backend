00ac1bd70cc60dfc0be7f60220ba868e6a147faf
Zach Perrault <zach.perrault@gmail.com>
2015-05-06 10:12:40 -0400

    💣💣💣💣💣💣

--- client/Posts/postsList.js
+++ client/Posts/postsList.js
@@ -1,3 +1,8 @@
+/* global displayName */
+/* global getPostLikeCount */
+/* global shortLinkMessage */
+/* global removePost */
+
 if (Meteor.isClient) {
 
 	var haveILikedThis = function (postID) {
@@ -11,25 +16,6 @@ if (Meteor.isClient) {
 		if(_.contains(likedBy, me))
 			return true;
 		return false;
-	};
-
-	var coordsRelativeToElement = function(element, event) {
-		var offset = $(element).offset();
-		var x = event.pageX - offset.left;
-		var y = event.pageY - offset.top;
-		return {
-			x: x,
-				y: y
-		};
-	};
-
-	var openCreateDialog = function(x, y) {
-		Session.set("createCoords", {
-			x: x,
-			y: y
-		});
-		Session.set("createError", null);
-		Session.set("showCreateDialog", true);
 	};
 
 	var timer = null;
@@ -53,7 +39,7 @@ if (Meteor.isClient) {
 					$('.content').scrollTop(event.state.scroll);
 					clearInterval(scrollWait);
 				}
-			}, 50)
+			}, 50);
 		}
 	};
 
@@ -66,48 +52,41 @@ if (Meteor.isClient) {
 	};
 
 	Template.liked_by.liked_list = function (postId) {
-		var liked_id;
-
-		var post = Posts.find(postId).fetch("likedBy")[0];
+		var post = Posts.find(postId).fetch('likedBy')[0];
 		if (!post) {
-			return "Error";
-		};
+			return 'Error';
+		}
 
-		usersThatLikedPost = post.likedBy;
-		var str ="";
+		var usersThatLikedPost = post.likedBy;
+		var str ='';
 		for (var i = usersThatLikedPost.length - 1; i >= 0; i--) {
 			if (usersThatLikedPost[i]) {
 				str += displayName(usersThatLikedPost[i]);
-			};
-			if(i!=0)
-				str+=", ";
-		};
+			}
+			if(i !== 0)
+				str += ', ';
+		}
 
-		if(usersThatLikedPost.length==0)
-			return "No one likes this yet";
+		if(usersThatLikedPost.length === 0)
+			return 'No one likes this yet';
 		return str;
 	};
-
 
 	Template.comment_area.helpers({
 		commentsCount: function(pid) {
 			return Comments.find({postId: pid}).count();
 		}
 	});
+
 	Template.like_area.helpers({
 		likesCount: function(pid) {
 			return getPostLikeCount(pid);
 		}
 	});
 
-
-	Template.comments_on.show = function (postId) {
-
-	}
-
 	Template.postTextShort.helpers({
 		ShortMessage: function (message) {
-			return ShortLinkMessage(message);
+			return shortLinkMessage(message);
 		}
 	});
 
@@ -119,18 +98,17 @@ if (Meteor.isClient) {
 
 	Template.femFeed.helpers({
 		showCreateDialog: function() {
-			return Session.get("showCreateDialog");
+			return Session.get('showCreateDialog');
 		}
 	});
 
-
 	Template.postlist.selected_name = function() {
-		var post = Posts.findOne(Session.get("selected_post"));
+		var post = Posts.findOne(Session.get('selected_post'));
 		return post && post.name;
 	};
 
 	Template.post.selected = function() {
-		return Session.equals("selected_post", this._id) ? "selected" : '';
+		return Session.equals('selected_post', this._id) ? 'selected' : '';
 	};
 
 	Template.post.events({
@@ -143,12 +121,12 @@ if (Meteor.isClient) {
 	Template.removePostButton.helpers({
 		myPost: function(postId) {
 			if(!postId)
-		return false;
-	var post = Posts.find(postId).fetch()[0]
-		if (!post) {
-			return false;
-		};
-	return post.ownedBy==Meteor.userId();
+				return false;
+			var post = Posts.find(postId).fetch()[0];
+			if (!post) {
+				return false;
+			}
+			return post.ownedBy === Meteor.userId();
 		}
 	});
 
@@ -160,9 +138,8 @@ if (Meteor.isClient) {
 
 	Template.removePost.events({
 		'click .remove': function(event, template) {
-			removePost(template.data)
+			removePost(template.data);
 		},
-
 	});
 
 	Template.like_area.events({
@@ -170,10 +147,10 @@ if (Meteor.isClient) {
 			var postId = this._id;
 			var userId = Meteor.userId();
 			if (!haveILikedThis(postId)) {
-				Meteor.call("like", postId, userId);
+				Meteor.call('like', postId, userId);
 			} else{
-				Meteor.call("unlike", postId, userId);
-			};
+				Meteor.call('unlike', postId, userId);
+			}
 		}
 	});
 }
