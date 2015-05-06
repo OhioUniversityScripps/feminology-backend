163e30d9a9f6e7f3bccacc5a3337c908221f8707
Zach Perrault <zach.perrault@gmail.com>
2015-05-06 10:53:51 -0400

    server/ done

--- server/server.js
+++ server/server.js
@@ -1,54 +1,56 @@
+/* global removeComment */
+
 if (Meteor.isServer) {
-  Meteor.startup(function() {
-    return Meteor.methods({
-      removeAllPosts: function() {
-        Comments.remove({});
-        return Posts.remove({});
-      },
-      removeAllUsers: function() {
-        return Meteor.users.remove({});
-      },
-      removeAllComments: function() {
-        return Comments.remove({});
-      },
-      removeUser: function(id) {
-        comments = Comments.find({ownedBy:id}).fetch();
+	Meteor.startup(function() {
+		return Meteor.methods({
+			removeAllPosts: function() {
+				Comments.remove({});
+				return Posts.remove({});
+			},
+			
+			removeAllUsers: function() {
+				return Meteor.users.remove({});
+			},
+		
+			removeAllComments: function() {
+				return Comments.remove({});
+			},
+			
+			removeUser: function(id) {
+				var comments = Comments.find({ownedBy:id}).fetch(); 
+				for (var i = 0; i < comments.length; i++) {
+					removeComment(comments[i]);
+				}
+				var thesePosts = Posts.find({ownedBy:id}).fetch();
+				for (var j = 0; j < thesePosts.length; j++) {
+					removeComment(thesePosts[j]);
+				}
+				return Meteor.users.remove({_id:id});
+			}
+		});
+	});
 
-        for (var i = 0; i < comments.length; i++) {
-          removeComment(comments[i]);
-        };
-        thesePosts = Posts.find({ownedBy:id}).fetch();
-        for (var i = 0; i < thesePosts.length; i++) {
-          removeComment(thesePosts[i]);
-        };
-        return Meteor.users.remove({_id:id});
-      }
-    });
-  });
+	Meteor.publish('posts', function () {
+		return Posts.find({});
+	});
 
-  Meteor.publish("posts", function () {
-    return Posts.find({});
-  });
+	Meteor.publish('comments', function() {
+		return Comments.find({});
+	});
 
-  Meteor.publish('comments', function(postId) {
-    return Comments.find({});
-  });
+	Meteor.publish('allUserData', function () {
+		return Meteor.users.find({});
+	});
 
-  Meteor.publish("allUserData", function () {
-      return Meteor.users.find({});
-  });
-
-
-  var TWITTER_KEY = process.env.TWITTER_KEY;
-  var TWITTER_SECRET = process.env.TWITTER_SECRET;
-  ServiceConfiguration.configurations.upsert(
-  { service: "twitter" },
-  {
-    $set: {
-      consumerKey: TWITTER_KEY,
-      loginStyle: "popup",
-      secret: TWITTER_SECRET
-    }
-  }
-);
+	var TWITTER_KEY = process.env.TWITTER_KEY;
+	var TWITTER_SECRET = process.env.TWITTER_SECRET;
+	ServiceConfiguration.configurations.upsert({
+		service: 'twitter'
+	},{
+		$set: {
+			consumerKey: TWITTER_KEY,
+			loginStyle: 'popup',
+			secret: TWITTER_SECRET
+		}
+	});
 }
