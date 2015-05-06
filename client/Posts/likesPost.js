6e680950c33e6b0611da1d2ff39a3e5b75be98b0
Zach Perrault <zach.perrault@gmail.com>
2015-05-06 10:03:29 -0400

    likesPost.js has been "linted"

--- client/Posts/likesPost.js
+++ client/Posts/likesPost.js
@@ -1,3 +1,7 @@
+/* global getCommentForPostFromUser: true */
+/* global displayName */
+/* global dateStringFixer */
+
 Template.likeList.usersWhoLikeThis = function (postId) {
 	var likerIds = Posts.findOne(postId).likedBy;
 	var likers = [];
@@ -6,23 +10,21 @@ Template.likeList.usersWhoLikeThis = function (postId) {
 		liker = Meteor.users.findOne(likerIds[i]);
 		liker.postId = postId;
 		likers[i]=liker;
-	};
+	}
 	return likers;
-}
+};
 
 Template.liker.user_name = function (user) {
-    return displayName(user);
-  };
-
+	return displayName(user);
+};
 
 Template.likeList.comments = function (postId) {
 	var commentIds = Posts.findOne(postId).comments;
-	var comments = [];
 	var comment; 
 	for (var i = commentIds.length - 1; i >= 0; i--) {
 		comment = Comments.findOne(commentIds[i]);
-	};
-}
+	}
+};
 
 Template.liker.hasComment = function (postId,userId) {
 	return getCommentForPostFromUser(postId,userId).length>0;
@@ -32,31 +34,28 @@ Template.showLastComment.latestTime = function (postId,userId) {
 	var comments = getCommentForPostFromUser(postId,userId);
 	var latestTime = new Date().setFullYear(1960);
 	var thisTime;
-	var latestDateString = "";
+	var latestDateString = '';
 	for (var i = comments.length - 1; i >= 0; i--) {
-		thisTime =new Date( comments[i].createdAt);
+		thisTime = new Date( comments[i].createdAt);
 		if (latestTime<thisTime) {
 			latestTime = thisTime;
 			latestDateString = comments[i].createdAt;
-		};
-	};
-	return DateStringFixer(latestDateString);
+		}
+	}
+	return dateStringFixer(latestDateString);
 };
-
-
-
 
 getCommentForPostFromUser = function (postId,userId) {
 	var commentIds = Posts.findOne(postId).comments;
 	var comments = [];
 	var comment; 
-	var count = 0
+	var count = 0;
 	for (var i = commentIds.length - 1; i >= 0; i--) {
 		comment = Comments.findOne(commentIds[i]);
-		if(comment.ownedBy == userId){
+		if(comment.ownedBy === userId){
 			comments[count] = comment;
 			count+=1;
 		}
 	}
-	return comments
-}
+	return comments;
+};
