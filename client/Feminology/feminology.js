dcf252d228498b8b9189b107334b5e897f675d12
Zach Perrault <zach.perrault@gmail.com>
2015-05-06 09:12:32 -0400

    A much cleaner feminology.js

--- client/Feminology/feminology.js
+++ client/Feminology/feminology.js
@@ -1,104 +1,57 @@
+'use strict';
+
 if (Meteor.isClient || Meteor.isCordova) {
-  Meteor.subscribe("allUserData");
-   Meteor.startup(function() {
-    AccountsEntry.config({
-      logo: '/img/longLogo.png',                  // if set displays logo above sign-in options
-      homeRoute: '/feed' ,                   // mandatory - path to redirect to after sign-out
-      dashboardRoute: '/feed',      // mandatory - path to redirect to after successful sign-in
-      showOtherLoginServices: true,      // Set to false to hide oauth login buttons on the signin/signup pages. Useful if you are using something like accounts-meld or want to oauth for api access
-    });
-});
-};
-
-
-whoami = function () {
-	return Meteor.userId();
+	Meteor.subscribe('allUserData');
+	Meteor.startup(function() {
+	});
 }
 
-haveILikedThis = function (postID) {
-	var post = Posts.findOne({_id:postID});
-	if (!post) {
-		return false;
-	};
-
-	likedBy = post.likedBy;
-  me = whoami();
-  if(_.contains(likedBy, me))
-  	return true;
-  return false
-}
-
-
-DateStringFixer = function (dateString) {
-	createdDate = moment(dateString);
-	Today = moment({hour: 0});
-	hoursDiff = Today.diff(createdDate,'hours');
-	daysDiff = Today.diff(createdDate,'days');
+var fixDateString = function (dateString) {
+	var createdDate = moment(dateString);
+	var Today = moment({hour: 0});
+	var hoursDiff = Today.diff(createdDate,'hours');
+	var daysDiff = Today.diff(createdDate,'days');
 	if (hoursDiff <=0) {
-		return moment(dateString).format('[Today at ]  h:mm a')
+		return moment(dateString).format('[Today at ]  h:mm a');
 	}
 	else if (hoursDiff <=24) {
-		return moment(dateString).format('[Yesterday at ]  h:mm a')
+		return moment(dateString).format('[Yesterday at ]  h:mm a');
 	}
 	else if (daysDiff<7) {
-		return moment(dateString).format('dddd, MMMM Do h:mm a')
+		return moment(dateString).format('dddd, MMMM Do h:mm a');
 	}
 	else if (moment(Today).isSame(createdDate,'year')) {
-		return moment(dateString).format('MMMM Do h:mm a')
+		return moment(dateString).format('MMMM Do h:mm a');
 	}
 	else{
-		return moment(dateString).format('MMMM Do YYYY h:mm a')
-	};
-}
-
-truncateStringInclusive = function (s,n) {
-	if (s) {
-		var cut = s.indexOf(' ', n);
-	} else {
-		var cut = -1;
+		return moment(dateString).format('MMMM Do YYYY h:mm a');
 	}
-  if(cut== -1) return s;
-  return s.substring(0, cut)+"...";
-}
-
-
-truncateStringExclusive = function(s,n){
-	if (s.length<=n) {
-		return s;
-	};
-  return s.substr(0, Math.min(s.substr(0,n).length, s.lastIndexOf(" ")));
-};
-
-var shortenLength = 100; // shorten to 100 characters
-
-ShortLinkMessage = function (message) {
-	return truncateStringInclusive(message,shortenLength);
 };
 
 Template.statusFooter.helpers({
-	me: function() { return whoami(); }
+	me: function() { return Meteor.userId(); }
 });
 
 Template.statusFooterProfileActive.helpers({
-	me: function() { return whoami(); }
+	me: function() { return Meteor.userId(); }
 });
 
 Template.statusFooterFeedActive.helpers({
-	me: function() { return whoami(); }
+	me: function() { return Meteor.userId(); }
 });
 
 Template.displayDate.helpers({
 	getFixedDate:function (date) {
-		return DateStringFixer(date);
+		return fixDateString(date);
 	}
 });
 
 Template.backButton.events({
-  'click .backButton': function (event) {
-    window.history.back();
-  }
+	'click .backButton': function () {
+		window.history.back();
+	}
 });
 
 Template.profileHeader.helpers({
-	isMyProfile: function(userId) {return userId === whoami(); }
+	isMyProfile: function(userId) {return userId === Meteor.userId(); }
 });
