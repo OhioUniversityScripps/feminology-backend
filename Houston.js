acb3c57e982bab4c04fe6cdd88f7a310e86ef129
Zach Perrault <zach.perrault@gmail.com>
2015-05-06 12:33:28 -0400

    What even was this...?

--- Houston.js
+++
@@ -1,7 +0,0 @@
-if (Meteor.isServer) {
-  // Houston.add_collection(Meteor.users);
-	// Houston.add_collection(Posts);
-	// Houston.add_collection(Comments);
-	// Houston.add_collection();
-	// Houston.add_collection(collection);
-};
