935a6766802e8b2e39f31dda351a637a004bc602
Zach Perrault <zach.perrault@gmail.com>
2015-05-06 09:11:11 -0400

    Linted Router js

--- Routing.js
+++ Routing.js
@@ -1,135 +1,99 @@
-/*
+'use strict';
 Router.configure({
-   layoutTemplate: 'layout',
-   loadingTemplate: 'loading',
-   waitOn: function() { return Meteor.subscribe('posts'); }
-});
-*/
-// Router.map(function() {
-//   this.route('postsPage', {path: '/'})
-//   this.route('about');
-//   this.route('profPage', {
-// 	  path: '/profile/:_id',
-// 	  data: function() { return Meteor.users.findOne(this.params._id); }
-// 	});
-// });
-
-Router.configure({
-  layout: 'layout',
-  loadingTemplate: 'loading',
-  notFoundTemplate: 'notFound',
-  waitOn: function() {
-  	return Meteor.subscribe('posts') && Meteor.subscribe('comments');
-  }
+	layout: 'layout',
+	loadingTemplate: 'loading',
+	notFoundTemplate: 'notFound',
+	waitOn: function() {
+		return Meteor.subscribe('posts') && Meteor.subscribe('comments');
+	}
 });
 
 Router.map(function () {
-  this.route('femFeed',{
-    path:'/feed',
-    waitOn: function () {
-      return Meteor.subscribe('posts');
-    }
-  });
+	this.route('femFeed',{
+		path:'/feed',
+		waitOn: function () {
+			return Meteor.subscribe('posts');
+		}
+	});
 
-  this.route('login',{
-    path:'/login',
-    // action: function () {
-		// Router.go('/sign-in');
-    // }
-  });
+	this.route('login',{
+		path:'/login',
+	});
 
-  this.route('signup', {
-    path: '/signup',
-  });
+	this.route('signup', {
+		path: '/signup',
+	});
 
-  this.route('termsOfService', {
-	  path: '/tos',
-  });
+	this.route('termsOfService', {
+		path: '/tos',
+	});
 
-  this.route('split',{
-    path:'/split'
-  });
+	this.route('split',{
+		path:'/split'
+	});
 
-  this.route('splash',{
-    path:'/',
-    action: function () {
-      Router.go('femFeed');
-    }
-  });
+	this.route('splash',{
+		path:'/',
+		action: function () {
+			Router.go('femFeed');
+		}
+	});
 
-  this.route('lgoin',{
-    path:'/profile',
-    action: function () {
-      Router.go('femFeed');
-    }
-  });
-  this.route('home',{
-    path:'/home',
-    action: function () {
-      Router.go('femFeed');
-    }
-  });
+	this.route('lgoin',{
+		path:'/profile',
+		action: function () {
+			Router.go('femFeed');
+		}
+	});
 
-  this.route('composePost', {
-    path: '/post/new',
-    template: 'composePost'
-  });
+	this.route('home',{
+		path:'/home',
+		action: function () {
+			Router.go('femFeed');
+		}
+	});
 
-  this.route('postPage',{
-    path:'/post/:_id',
-    waitOn: function() {
-		    return Meteor.subscribe('comments', this.params._id);
-    },
-    data: function () {
-      return Posts.findOne({_id:this.params._id});
-    }
-  });
+	this.route('composePost', {
+		path: '/post/new',
+		template: 'composePost'
+	});
 
-  this.route('likesPost',{
-    path:'/post/:_id/likes',
-    data: function () {
-      return Posts.findOne({_id:this.params._id});
-    }
-  });
+	this.route('postPage',{
+		path:'/post/:_id',
+		waitOn: function() {
+			return Meteor.subscribe('comments', this.params._id);
+		},
+		data: function () {
+			return Posts.findOne({_id:this.params._id});
+		}
+	});
 
-  this.route('profileId',{
-    path:'/profile/:_id',
-    template: "profile",
-    data: function () {
-      return Meteor.users.findOne({_id:this.params._id});
-    }
-  });
+	this.route('likesPost',{
+		path:'/post/:_id/likes',
+		data: function () {
+			return Posts.findOne({_id:this.params._id});
+		}
+	});
 
+	this.route('profileId',{
+		path:'/profile/:_id',
+		template: 'profile',
+		data: function () {
+			return Meteor.users.findOne({_id:this.params._id});
+		}
+	});
 
-  this.route('profile',{
-    path:'/profile/:ownedBy',
-    data: function () {
-      return Meteor.users.findOne({_id:this.params.ownedBy});
-    }
-  });
-  this.route('editProfile',{
-    path:'/profile/:_id/edit',
-    data: function () {
-      return Meteor.users.findOne({_id:this.params._id});
-    }
-  });
-  this.route('browser',{
-    action: function () {
-      var generalURL = "https://feminologywiki.com/";
-      showWebViewBrowser(generalURL);
-    }
-  });
+	this.route('profile',{
+		path:'/profile/:ownedBy',
+		data: function () {
+			return Meteor.users.findOne({_id:this.params.ownedBy});
+		}
+	});
+
+	this.route('editProfile',{
+		path:'/profile/:_id/edit',
+		data: function () {
+			return Meteor.users.findOne({_id:this.params._id});
+		}
+	});
 });
-
-showWebViewBrowserCurrent = function () {
-  showWebViewBrowser("https://feminologywiki.com/index.php?title=Current_Events");
-}
-
-
-showWebViewBrowser = function (url) {
-    if (!url) {
-      url = "https://feminologywiki.com/";
-    };
-    var ref = window.open(url,"_system");
-    // var ref = window.open("http://www.facebook.com","_blank","location=yes");
-}
