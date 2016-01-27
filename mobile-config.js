7c5c57b3dc990aa5799bf8886859218c456bb216
Zach Perrault <zach.perrault@gmail.com>
2016-01-27 08:28:41 -0500

    Updates icons and splash screens to support Android

--- mobile-config.js
+++ mobile-config.js
@@ -23,6 +23,10 @@ App.icons({
 	'iphone': 'resources/icons/icon-iphone.png',
 	'iphone_2x': 'resources/icons/icon-iphone_2x.png',
 	'iphone_3x': 'resources/icons/icon-iphone_3x.png',
+	'android_ldpi': 'resources/icons/icon-android-ldpi.png',
+  'android_mdpi': 'resources/icons/icon-android-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-android-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-android-xhdpi.png'
 });
 
 App.launchScreens({
@@ -32,4 +36,8 @@ App.launchScreens({
 	'iphone5': 'resources/splash/splash-iphone5.png',
 	'iphone6': 'resources/splash/splash-iphone6.png',
 	'iphone6p_portrait': 'resources/splash/splash-iphone6p_portrait.png',
+	'android_ldpi_portrait': 'resources/splash/splash-android-ldpi.png',
+	'android_mdpi_portrait': 'resources/splash/splash-android-mdpi.png',
+	'android_hdpi_portrait': 'resources/splash/splash-android-hdpi.png',
+	'android_xhdpi_portrait': 'resources/splash/splash-android-xhdpi.png'
 });
